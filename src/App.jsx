2723a7f435165023aca06e6fee0ff6727bd3b007
Andrew P <andreich1980@gmail.com>
2021-02-24 12:53:54 +0300

    Remove unused dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from 'react-router-dom'
 
 import Login from './pages/Login'
 import Logo from './components/Logo'
@@ -12,14 +7,7 @@ export default function App() {
     return (
         <div className="min-h-screen backrgound flex flex-col sm:justify-center items-center py-6 sm:py-0">
             <Logo className="w-16 h-16 mx-auto text-white mb-6" />
-
-            <Router>
-                <Switch>
-                    <Route path="/">
-                        <Login />
-                    </Route>
-                </Switch>
-            </Router>
+            <Login />
         </div>
     )
 }
