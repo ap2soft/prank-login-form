3f6e743e1e6b8eaccceacf9202c81e4e99aa1d35
Andrew P <andreich1980@gmail.com>
2021-02-24 12:57:22 +0300

    Redirect if user tries offered password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,16 +8,30 @@ export default class Login extends React.Component {
         super(props)
 
         this.state = {
+            email: null,
             hasErrors: false,
-            name: 'John Smith',
-            email: 'john.smith@example.com'
+            randomMame: null,
+            randomEmail: null,
         }
 
+        this.handleInputChange = this.handleInputChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     async handleSubmit(e) {
         e.preventDefault()
+
+        if (this.state.randomEmail && this.state.randomEmail === this.state.email) {
+            location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+            return
+        }
 
         await this.randomize()
 
@@ -32,8 +46,8 @@ export default class Login extends React.Component {
         let domain = this.randomItemOf(await this.fetchData('domains.txt'))
 
         this.setState({
-            name: this.capitalize(firstName) + ' ' + this.capitalize(lastName),
-            email: firstName + '.' + lastName + '@' + domain,
+            randomName: this.capitalize(firstName) + ' ' + this.capitalize(lastName),
+            randomEmail: firstName + '.' + lastName + '@' + domain,
         })
     }
 
@@ -63,8 +77,8 @@ export default class Login extends React.Component {
 
                 {this.state.hasErrors &&
                     <div className="mt-4 bg-red-200 bg-opacity-70 text-red-900 leading-tight rounded text-center p-4">
-                        You entered <span className="font-semibold">{this.state.name}</span>'s password.
-                        Maybe your email is <span className="font-semibold">{this.state.email}</span>?
+                        You entered <span className="font-semibold">{this.state.randomName}</span>'s password.
+                        Maybe your email is <span className="font-semibold">{this.state.randomEmail}</span>?
                     </div>
                 }
 
@@ -73,9 +87,12 @@ export default class Login extends React.Component {
                         <label htmlFor="email" className="block text-white text-sm">Email</label>
                         <input
                             id="email"
+                            name="email"
                             type="email"
                             className="mt-1 w-full bg-gray-300 focus:bg-white text-gray-700 border-0 rounded focus:ring-0 transition duration-150 ease-in-out"
                             required
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
                         />
                     </div>
 
@@ -83,6 +100,7 @@ export default class Login extends React.Component {
                         <label htmlFor="password" className="block text-white text-sm">Password</label>
                         <input
                             id="password"
+                            name="password"
                             type="password"
                             className="mt-1 w-full bg-gray-300 focus:bg-white text-gray-700 border-0 rounded focus:ring-0 transition duration-150 ease-in-out"
                             required
